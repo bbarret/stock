bb558cfcd939ae41f2948bae1bbaf72e8e55e72b
bertrand <bertrand.barret@gmail.com>
2020-03-16 15:25:48 +0100

    suppression des tests

--- src/app/directives/stop-click-event-propagation.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { StopClickEventPropagationDirective } from './stop-click-event-propagation.directive';
-
-describe('StopClickEventPropagationDirective', () => {
-  it('should create an instance', () => {
-    const directive = new StopClickEventPropagationDirective();
-    expect(directive).toBeTruthy();
-  });
-});
