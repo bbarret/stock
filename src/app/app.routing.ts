84305bd6d270cf05bf312e60f9c5b58d0f49950d
bertrand <bertrand.barret@gmail.com>
2020-03-18 13:17:34 +0100

    page par défaut /articles

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: "", redirectTo: "/overview", pathMatch: "full" },
+  { path: "", redirectTo: "/articles", pathMatch: "full" },
   { path: "articles", loadChildren: './inventaire/inventaire.module#InventaireModule'},
   { path: "lieux", loadChildren: './lieux/lieux.module#LieuxModule' }
 ];
